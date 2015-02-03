515fe3760a642c438899536b643773bd6097e36b
mcin0244 <mcin0244@algonquinlive.com>
2015-02-03 18:09:32 -0500

    feat: adding responsive navigation

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,5 @@
+// Toggle Nav JS
+
 	var navTop = document.querySelector('.nav-top');
 	
 	document.querySelector('.nav-btn').addEventListener('click', function(e){
@@ -7,10 +9,10 @@
 		if (navTop.getAttribute('data-state') == 'expanded'){
 			
 			navTop.setAttribute('data-state', 'collapsed');
-		  this.setAttribute('data-state', 'inactive');
+		
 		}else{
 		
 			navTop.setAttribute('data-state', 'expanded');
-	    this.setAttribute('data-state', 'active');
+		
 		}
 	});
